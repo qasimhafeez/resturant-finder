64b931a87f7f9a860cece4727a422889d70df9d7
Qasim Hafeez <qasimh556@gmail.com>
2021-02-02 21:26:07 +0400

    backend

---
+++ backend/src/data.js
@@ -0,0 +1,22 @@
+export const restaurants = [
+	{
+		restaurantsName: 'katis restaurant',
+		log: '25.3259° N',
+		Lat: '55.3764° E',
+		address:
+			'Al Noor Sharjah - Al Khan St - Al Majaz - Sharjah - United Arab Emirates',
+	},
+	{
+		restaurantsName: 'chin chin sharjah muweilah',
+		log: '25.3053° N',
+		Lat: '55.4688° E',
+		address: ' Muweilah - University City Rd - Sharjah - United Arab Emirates',
+	},
+	{
+		restaurantsName: 'Al Qubtan Restaurant',
+		log: '25.3957° N',
+		Lat: '55.4231° E',
+		address:
+			'Sheraton Sharjah Beach Resort & Spa, Al Muntazah Street - Sharjah - United Arab Emirates',
+	},
+]
